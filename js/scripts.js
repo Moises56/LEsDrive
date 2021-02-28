2e55e246970efa36d6d5cc043be1c4f8836806a8
unknown <moiavil56@gmail.com>
2021-02-27 22:09:24 -0600

    Version 1.1 Landing Page

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,7 @@
-/*!
-    * Start Bootstrap - Creative v6.0.4 (https://startbootstrap.com/theme/creative)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
-    */
-    (function($) {
-  "use strict"; // Start of use strict
 
-  // Smooth scrolling using jQuery easing
+    (function($) {
+  "use strict";
+  
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
